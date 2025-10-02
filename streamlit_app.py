5514625e8fbb2cbf9758b5d5887c95f0c623675c
yayyw <160223276+yayyw@users.noreply.github.com>
2025-10-02 15:02:39 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-mport streamlit as st
-
 st.title("buy your wallet!!")
 
 number = st.number_input("Enter the quantity of wallets, $20 each",min_value=0, max_value=10, step=1, key="wallets")
