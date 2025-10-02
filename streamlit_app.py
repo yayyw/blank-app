c50fa69c9329a08726837e7c105dacd1078165c3
yayyw <160223276+yayyw@users.noreply.github.com>
2025-10-02 15:20:10 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,15 @@ if "button_press" not in st.session_state:
 if st.button("Customise your wallet"):
     st.session_state.button_press = True
     
+customise = ['engraving', 'material']
+if "button_press" not in st.session_state:
+    st.session_state.button_press = False
+
+if st.button("engraving"):
+    st.session_state.button_press = True
+    st.text_input('what are you engraving?', key = 'engraving')
+    price_of_wallet += 10
+    
 amount_of_wallet = number
 if amount_of_wallet > 0 and st.session_state.button_press:
     choice = st.selectbox("Select one material", ["leather", "nylon", "canvas"])
