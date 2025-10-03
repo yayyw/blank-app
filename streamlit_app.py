9961cd1d048735547ff4f24affb9d757c105847a
yayyw <160223276+yayyw@users.noreply.github.com>
2025-10-04 00:50:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,26 +22,26 @@ if "engraving_pressed" not in st.session_state:
 amount_of_wallet = number
 if amount_of_wallet > 0 and st.session_state.customise_pressed:
     choice = st.selectbox("Select one material", ["leather", "nylon", "canvas"])
-
-    if choice == "leather":
-        st.write("For leather add $50")
-        price_of_wallet += 50
-    elif choice == "nylon":
-        st.write("For nylon add $25")
-        price_of_wallet += 25
-    else:
-        st.write("Canvas adds $0")
-        
-        if st.button("Customise your wallet"):
-            st.session_state.customise_pressed = True #when button pressed
     
-        if st.button("Add engraving"):
-            st.session_state.engraving_pressed = True
+    while st.button("Customise your wallet"):
+            st.session_state.customise_pressed = True #when button pressed
+        
+        if choice == "leather":
+            st.write("For leather add $50")
+            price_of_wallet += 50
+        elif choice == "nylon":
+            st.write("For nylon add $25")
+            price_of_wallet += 25
+        else:
+            st.write("Canvas adds $0")
+    
+            if st.button("Add engraving"):
+                st.session_state.engraving_pressed = True
 
-        if st.session_state.engraving_pressed:
-            st.write("Engraving adds $10")
-            engraving_text = st.text_input("What would you like engraved?", key="engraving_text")
-            price_of_wallet += 10
+            if st.session_state.engraving_pressed:
+                st.write("Engraving adds $10")
+                engraving_text = st.text_input("What would you like engraved?", key="engraving_text")
+                price_of_wallet += 10
 
 st.write("Your total =", price_of_wallet)
 
