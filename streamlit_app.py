36a94ecc6619383cfd0e8c9b69bc40e4631791e2
yayyw <160223276+yayyw@users.noreply.github.com>
2025-10-05 18:41:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,50 +7,52 @@ price_of_wallet = number*20
 
 #discount price for tiered pricing 
 
-customise = ['size', 'material', 'engraving'] #add colours appeal to more ages bro idk anymore, list
-st.write('customisations available:')
-for item in customise: #for loop
-    st.write(item)
+for i in range(number):
+
+    customise = ['size', 'material', 'engraving'] #add colours appeal to more ages bro idk anymore, list
+    st.write('customisations available:')
+    for item in customise: #for loop
+        st.write(item)
     
-if "customise_pressed" not in st.session_state: #visibility of button
-    st.session_state.customise_pressed = False
-if "engraving_pressed" not in st.session_state:
-    st.session_state.engraving_pressed = False
+    if "customise_pressed" not in st.session_state: #visibility of button
+        st.session_state.customise_pressed = False
+    if "engraving_pressed" not in st.session_state:
+        st.session_state.engraving_pressed = False
 
-if st.button("Customise your wallet"):
-    st.session_state.customise_pressed = True #when button pressed
+    if st.button("Customise your wallet"):
+        st.session_state.customise_pressed = True #when button pressed
 
-amount_of_wallet = number
-if amount_of_wallet > 0 and st.session_state.customise_pressed:
-    st.image('https://pngimg.com/uploads/wallet/wallet_PNG77078.png', caption =None, width = 100)
-    size = st.selectbox("Select a size", ["small", "medium", "large"]) #reyna's code
-    if size == "medium":
-        st.write("For medium add $10")
-        price_of_wallet += 10
-    elif size == "large":
-        st.write("For large add $20")
-        price_of_wallet += 20
-    else:
-        st.write("small adds $0")
+    amount_of_wallet = number
+    if amount_of_wallet > 0 and st.session_state.customise_pressed:
+        st.image('https://pngimg.com/uploads/wallet/wallet_PNG77078.png', caption =None, width = 100)
+        size = st.selectbox("Select a size", ["small", "medium", "large"]) #reyna's code
+        if size == "medium":
+            st.write("For medium add $10")
+            price_of_wallet += 10
+        elif size == "large":
+            st.write("For large add $20")
+            price_of_wallet += 20
+        else:
+            st.write("small adds $0")
         
-    choice = st.selectbox("Select one material", ["leather", "nylon", "canvas"])
+        choice = st.selectbox("Select one material", ["leather", "nylon", "canvas"])
         
-    if choice == "leather":
-        st.write("For leather add $50")
-        price_of_wallet += 50
-    elif choice == "nylon":
-        st.write("For nylon add $25")
-        price_of_wallet += 25
-    else:
-        st.write("Canvas adds $0")
+        if choice == "leather":
+            st.write("For leather add $50")
+            price_of_wallet += 50
+        elif choice == "nylon":
+            st.write("For nylon add $25")
+            price_of_wallet += 25
+        else:
+            st.write("Canvas adds $0")
     
-    if st.button("Add engraving"):
-        st.session_state.engraving_pressed = True
+        if st.button("Add engraving"):
+            st.session_state.engraving_pressed = True
 
-    if st.session_state.engraving_pressed:
-        st.write("Engraving adds $10")
-        engraving_text = st.text_input("What would you like engraved?", key="engraving_text")
-        price_of_wallet += 10
+        if st.session_state.engraving_pressed:
+            st.write("Engraving adds $10")
+            engraving_text = st.text_input("What would you like engraved?", key="engraving_text")
+            price_of_wallet += 10
 
 st.write("Your total =", price_of_wallet)
 
