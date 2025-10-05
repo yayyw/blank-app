22f7a802e8eefde6968589327f30cc052fdcaad9
yayyw <160223276+yayyw@users.noreply.github.com>
2025-10-05 18:23:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ if st.button("Customise your wallet"):
 
 amount_of_wallet = number
 if amount_of_wallet > 0 and st.session_state.customise_pressed:
-    st.image(https://sl.bing.net/eht7FFpNZ6q, caption ="small")
+    st.image('https://sl.bing.net/eht7FFpNZ6q', caption ="small", use_column_width=True)
     size = st.selectbox("Select a size", ["small", "medium", "large"]) #reyna's code
     if choice == "medium":
         st.write("For medium add $10")
